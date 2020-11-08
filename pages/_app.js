e4828735b6e78df7e3f5fbd65a788b3d3fb24b4d
Jimmy404 <bruno_pier@hotmail.com>
2020-11-08 10:13:18 -0300

    Modify the original code of next.js - Create the MyApp component and add AuthState and props

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+/* -- Default function of _app.js --
+
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
 }
 
 export default MyApp
+
+*/
+
+import React from 'react';
+import AuthState from '../context/auth/authState';
+
+const MyApp = ({ Component, pageProps }) => {
+    return(
+        <AuthState>
+            <Component {...pageProps}/>
+        </AuthState>
+    )
+}
+
+export default MyApp;
