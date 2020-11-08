70bfa02fb1720f8034e34edd46d36e63ce8e75bc
Jimmy404 <bruno_pier@hotmail.com>
2020-11-08 10:29:22 -0300

    Create state for context - authState

--- context/auth/authState.js
+++ context/auth/authState.js
@@ -1,11 +1,26 @@
 import React, { useReducer } from 'react';
 import authContext from './authContext';
+import authReducer from './authReducer';
 
 const AuthState = ({children}) => {
 
+  const initialState = {
+    token: '',
+    authenticated: null,
+    user: null,
+    message: null
+  }
+
+  const [ state, dispath ] = useReducer(authReducer, initialState);
+
   return(
     <authContext.Provider
-      value={{}}
+      value={{
+        token: state.token,
+        authenticated: state.authenticated,
+        user: state.user,
+        message: state.message
+      }}
     >
       {children}
     </authContext.Provider>
