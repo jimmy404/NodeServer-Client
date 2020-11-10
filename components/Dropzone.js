f0250abdc442b386f0b53145e027469a40df6329
Jimmy404 <bruno_pier@hotmail.com>
2020-11-10 13:27:42 -0300

    Adds and show the button to create a link conditionally if a valid file is uploaded

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -24,24 +24,41 @@ const Dropzone = () => {
     </li>
   ));
 
+  const createLink = () => {
+    console.log('Creando enlace')
+  }
+
   return(
     <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
-      <ul>
-        {files}
-      </ul>
-      <div {...getRootProps({ className: 'dropzone w-full py-32' })}>
-        <input className="h-100" {...getInputProps()} />
-          {
-            isDragActive ? <p className="text-2xl text-center text-gray-600">Drop the file</p> :
-            <div className="text-center">
-              <p className="text-2xl text-center text-gray-600">Select a file or drop it here</p>
-              <button
-                className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
-                type="button"
-              >Select files to upload</button>
-            </div>
-          }
-      </div>
+      { acceptedFiles.length > 0 ? (
+        <div className="mt-10 w-full">
+          <h4 className="text-2xl font-bold text-center mb-4">Files</h4>
+          <ul>
+            {files}
+          </ul>
+          <button
+            type="button"
+            className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
+            onClick={ () => createLink() }
+          >
+            Create Link
+          </button>
+        </div>
+      ) : (
+        <div {...getRootProps({ className: 'dropzone w-full py-32' })}>
+          <input className="h-100" {...getInputProps()} />
+            {
+              isDragActive ? <p className="text-2xl text-center text-gray-600">Drop the file</p> :
+              <div className="text-center">
+                <p className="text-2xl text-center text-gray-600">Select a file or drop it here</p>
+                <button
+                  className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
+                  type="button"
+                >Select files to upload</button>
+              </div>
+            }
+        </div>
+      )}
     </div>
   );
 }
