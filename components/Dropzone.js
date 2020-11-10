e9d5953ec2afb18180ef98bbb3205415f113f6fc
Jimmy404 <bruno_pier@hotmail.com>
2020-11-10 12:48:00 -0300

    Adds - list the file to upload and showing the name and size

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -17,8 +17,18 @@ const Dropzone = () => {
 
   const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({onDrop});
 
+  const files = acceptedFiles.map( file => (
+    <li key={file.lastModified} className="bg-white flex-1 p-3 mb-4 shadow-lg rounded">
+      <p className="font-bold text-xl">{file.path}</p>
+      <p className="text-sm text-gray-500">{ (file.size / Math.pow(1024, 2)).toFixed(2) } Mb</p>
+    </li>
+  ));
+
   return(
     <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
+      <ul>
+        {files}
+      </ul>
       <div {...getRootProps({ className: 'dropzone w-full py-32' })}>
         <input className="h-100" {...getInputProps()} />
           {
