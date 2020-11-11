1680fe20dc0f1980d1e2d6fe4597cae262d02cd9
Jimmy404 <bruno_pier@hotmail.com>
2020-11-11 19:06:19 -0300

    Adds the code for getServerSidePaths / Props

--- pages/links/[link].js
+++ pages/links/[link].js
@@ -1,6 +1,29 @@
 import Layout from '../../components/Layout';
+import clienteAxios from '../../config/axios';
 
-export default () => {
+export async function getServerSideProps({params}) {
+  const { link } = params;
+  const result = await clienteAxios.get(`/api/links/${link}`);
+  return {
+    props: {
+      link: result.data
+    }
+  }
+}
+
+export async function getServerSidePaths() {
+  const links = await clienteAxios.get('/api/links');
+
+  return {
+    path: links.data.links.map( link => ({
+      params: { link: link.url}
+    })),
+    fallback: false
+  }
+}
+
+export default ({link}) => {
+  console.log(link)
   return(
     <Layout>
       <h1>From [link].js</h1>
