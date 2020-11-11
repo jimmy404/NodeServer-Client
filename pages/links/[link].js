720d3b899db12c969c9c2cf2b7e554ba97480fd9
Jimmy404 <bruno_pier@hotmail.com>
2020-11-11 19:27:50 -0300

    Adds code to download the file associated with a URL - without removing it from the DB yet

--- pages/links/[link].js
+++ pages/links/[link].js
@@ -26,7 +26,12 @@ export default ({link}) => {
   console.log(link)
   return(
     <Layout>
-      <h1>From [link].js</h1>
+      <h1 className="text-4xl text-center text-gray-700">Download your file:</h1>
+      <div className="flex items-center justify-center mt-10">
+        <a
+          href={`${process.env.backendURL}/api/files/${link.file}`}
+          className="bg-red-500 text-center px-10 py-3 rounded uppercase font-bold text-white cursor-pointer">Here</a>
+      </div>
     </Layout>
   );
 }
