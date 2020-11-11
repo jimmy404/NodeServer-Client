c01b5b493eefcf38f0e92c4f392da3d7692fef43
Jimmy404 <bruno_pier@hotmail.com>
2020-11-11 12:04:52 -0300

    Adds upload file case and loading message

--- context/app/appState.js
+++ context/app/appState.js
@@ -4,16 +4,22 @@ import appReducer from './appReducer';
 import {
   SHOW_ALERT,
   CLEAN_ALERT,
+  UPLOAD_FILE,
   UPLOAD_FILE_SUCCESS,
   UPLOAD_FILE_ERROR,
   CREATE_LINK_SUCCESS,
   CREATE_LINK_ERROR
 } from '../../types';
 
+import clienteAxios from '../../config/axios';
+
 const AppState = ({children}) => {
 
   const initialState = {
     message_file: null,
+    name: '',
+    original_name: '',
+    loading: null
   }
 
   const [ state, dispatch ] = useReducer( appReducer, initialState );
@@ -31,11 +37,40 @@ const AppState = ({children}) => {
     }, 5000);
   }
 
+  const uploadFile = async (formData, fileName ) => {
+
+    dispatch({
+      type: UPLOAD_FILE
+    })
+
+    try {
+      const result = await clienteAxios.post('/api/files', formData);
+      dispatch({
+        type: UPLOAD_FILE_SUCCESS,
+        payload: {
+          name: result.data.file,
+          original_name: fileName
+        }
+      })
+      console.log(result)
+    } catch (error) {
+      dispatch({
+        type: UPLOAD_FILE_ERROR,
+        payload: error.response.data.msg
+      })
+      console.log(error)
+    }
+  }
+
   return(
     <appContext.Provider
       value={{
         message_file: state.message_file,
-        showAlert
+        name: state.name,
+        original_name: state.original_name,
+        loading: state.loading,
+        showAlert,
+        uploadFile
       }}
     >
       {children}
