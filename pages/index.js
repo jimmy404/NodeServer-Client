26468365fce3a763f64996381ecfa11690bc7911
Jimmy404 <bruno_pier@hotmail.com>
2020-11-11 13:47:30 -0300

    Adds url to copy & botton that copies the link

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Index = () => {
   const { authenticatedUser } = AuthContext;
 
   const AppContext = useContext(appContext);
-  const { message_file } = AppContext;
+  const { message_file, url } = AppContext;
 
   useEffect(() => {
     authenticatedUser();
@@ -21,19 +21,35 @@ const Index = () => {
   return(
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
-        { message_file && <Alert /> }
-        <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
-          <Dropzone />
-          <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
-            <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">Share files easily and privately</h2>
-            <p className="text-lg leading-loose">
-              <span className="text-red-500 font-bold">ReactNodeSend </span>Allows you to share files with end-to-end encryption and a file that is deleted after it is downloaded. You can keep what you share private and make sure your stuff doesn't stay online forever.
-            </p>
-            <Link href="/createaccount">
-              <a className="text-red-500 font-bold text-lg hover:text-red-700">Create an account for more benefits.</a>
-            </Link>
-          </div>
-        </div>
+      { url ? (
+        <>
+          <p className="text-center text-2xl mt-10">
+            <span className="font-bold text-red-700 text-3xl uppercase">Your URL is: </span>
+            {`${process.env.frontendURL}/links/${url}`}
+          </p>
+          <button
+            type="button"
+            className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold mt-10"
+            onClick={() => navigator.clipboard.writeText(`${process.env.frontendURL}/links/${url}`)}
+          >Copy link</button>
+        </>
+        ) : (
+          <>
+            { message_file && <Alert /> }
+            <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+              <Dropzone />
+              <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+                <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">Share files easily and privately</h2>
+                <p className="text-lg leading-loose">
+                  <span className="text-red-500 font-bold">ReactNodeSend </span>Allows you to share files with end-to-end encryption and a file that is deleted after it is downloaded. You can keep what you share private and make sure your stuff doesn't stay online forever.
+                </p>
+                <Link href="/createaccount">
+                  <a className="text-red-500 font-bold text-lg hover:text-red-700">Create an account for more benefits.</a>
+                </Link>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </Layout>
   );
